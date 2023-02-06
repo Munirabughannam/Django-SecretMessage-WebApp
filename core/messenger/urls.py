cc96fbf79cdb7719c3541501a7d46a0adbea0db3
munir <munirabughannam@gmail.com>
2023-02-06 01:26:55 -0800

    mapped the urls

--- core/messenger/urls.py
+++ core/messenger/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
+
 
 urlpatterns = [
-
+    path('', views.messageprompt, name='base'),
+    path('reveal', views.generation_view, name='generation'),
+    path('reveal/message/<str:secret_id>', views.reveal_message, name='reveal'),
 ]
